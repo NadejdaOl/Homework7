e5d95de015662c0441b1c8c580996a79b40190cc
NadejdaOl <135122814+NadejdaOl@users.noreply.github.com>
2023-06-26 08:18:26 +0200

    Add files via upload
    Initial commit

---
+++ Homework_7/scripts/script.js
@@ -0,0 +1,37 @@
+
+// for (let i = 0; i <= 10; i = i + 2 {
+    // console.log(i);
+// };
+
+
+// for (let i = 55; i > 20; i = i - 1) {
+   // console.log(i)
+// }
+
+
+// const numbers = [3, 5, 11, 2, 8, 1, 6]
+// for (let i = 0; i <numbers.length; i = i + 1) {
+    // console.log(numbers[i])
+// }
+
+
+// const numbers = [3, 5, 11, 2, 8, 1, 6]
+// const numbers_squared = []
+// numbers_squared.push(numbers[0]**2, numbers[1]**2, numbers[2]**2, numbers[3]**2, numbers[4]**2, numbers[5]**2, numbers[6]**2)
+    // console.log(numbers_squared)
+
+    // onst numbers_squared = [9, 25, 121, 4, 64, 1, 36 ]
+    // const last_elem = [];
+    // last_elem.push(numbers_squared[6])
+        // console.log(last_elem)
+
+
+        const user = {
+            first_name: 'Ivan', 
+            last_name: 'Ivanov', 
+            age: 20, 
+            salary: 500
+        }
+
+        console.log(`User’s name is ${user.first_name} ${user.last_name}`)
+        console.log(`He is ${user.age} years old`)
